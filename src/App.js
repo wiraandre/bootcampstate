3fe94077fe5ee3a21ee83c54ef47ce90e42e6c0e
unknown <andre wira>
2018-07-30 16:24:24 +0700

    benahi lagi di bagian alertnya

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header wow="asaks" isi="aaa" />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Counter />
+      </div>
+    );
+  }
+}
+
+class Counter extends Component {
+  state = {
+    angka: 0
+  };
+
+  tambah = () => {
+    this.setState({
+      angka: this.state.angka + 1
+    });
+  };
+
+  kurang = () => {
+    if(this.state.angka<0){
+      alert"angka tidak boleh lebih kecil dari 0";
+    }else{
+      this.setState({
+        angka: this.state.angka - 1
+    }
+    
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.angka}</h1>
+
+        <button onClick={this.tambah}>tambah</button>
+        <button onClick={this.kurang}>kurang</button>
+      </div>
+    );
+  }
+}
